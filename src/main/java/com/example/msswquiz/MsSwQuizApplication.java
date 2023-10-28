83a4b9e61782b78d575c5a38bf418b2971b3869a
Rickson <ricksonrocha0@gmail.com>
2023-10-28 15:06:32 -0300

    feat: :sparkles: criação do projeto

---
+++ src/main/java/com/example/msswquiz/MsSwQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.msswquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSwQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSwQuizApplication.class, args);
+	}
+
+}
